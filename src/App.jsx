c47d43c9a9147baade93a8c268c447b18bf2526d
H4xN-Harsh <harshxvillen74@gmail.com>
2025-07-27 12:10:44 +0530

    2nd day

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import Canvas from './Canvas'
+import data from './data'
 
 function App() {
-  return (
-    <div className='w-full min-h-screen bg-black text-white'>
-      hii
+  return (<>
+    <div className='w-full relative min-h-screen text-white'>
+      {data[0].map((canvasdets,index)=>(
+        <Canvas details={canvasdets}/>
+      ))
+
+      }
     </div>
+    <div className='w-full relative min-h-screen text-white'>
+      {data[1].map((canvasdets,index)=>(
+        <Canvas details={canvasdets}/>
+      ))
+
+      }
+    </div>
+    <div className='w-full relative min-h-screen text-white'>
+      {data[2].map((canvasdets,index)=>(
+        <Canvas details={canvasdets}/>
+      ))
+
+      }
+    </div>
+    </>
   )
 }
 
